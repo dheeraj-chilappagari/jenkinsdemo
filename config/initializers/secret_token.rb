3c3321c50953d92734024c3d079f5557f54874de
Dheeraj Kumar <dheeraj.chilappagari@valuelabs.com>
2018-11-30 12:08:39 +0530

    jenkins demo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jenkinsdemo::Application.config.secret_key_base = '7e6785568d30a92ec6120226d87d8175436eddfb840bbe2443e1645432cfb9ee3e4261f79b027002860f7dea2f6b3921af7593ced14c984edee362aa57c85a4e'
